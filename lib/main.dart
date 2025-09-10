2464997fbe5511663ccba762e010130d77b7aa11
juliannGabrielDev <julian.alejandro.gabriel@gmail.com>
2025-09-09 23:29:46 -0600

    Update app color scheme to use light green and add launch configuration file

--- lib/main.dart
+++ lib/main.dart
@@ -3,26 +3,22 @@ import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  // En la función main colocamos la ejecución de un widget
   runApp(const MyApp());
 }
 
-/// A simple Flutter app that displays 'Hello World'.
+/// The main application widget.
 class MyApp extends StatelessWidget {
   /// Creates a [MyApp] widget.
   const MyApp({super.key});
 
   static final _defaultLightColorScheme = ColorScheme.fromSeed(
-    seedColor: Colors.blue,
+    seedColor: Colors.lightGreen,
   );
   static final _defaultDarkColorScheme = ColorScheme.fromSeed(
-    seedColor: Colors.grey,
+    seedColor: Colors.lightGreen,
     brightness: Brightness.dark,
   );
 
-  // El key permite identificar un widget
-  // Los widgets deben tener un Widget build
-  // BuildContext genera el árbol de widgets
   @override
   Widget build(BuildContext context) => DynamicColorBuilder(
     builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) =>
